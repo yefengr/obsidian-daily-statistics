c3bbdfc94b2cba4c21d8fa9c3659ba87f01c6d91
yefengr <yefengr@qq.com>
2024-07-15 17:10:33 +0800

    代码优化

--- src/ui/setting/SampleSettingTab.ts
+++ src/ui/setting/SampleSettingTab.ts
@@ -66,24 +66,24 @@ export class SampleSettingTab extends PluginSettingTab {
             await this.plugin.saveSettings();
           })
       );
-    new Setting(containerEl)
-      .setName("每日目标")
-      .setDesc("设置每日目标。修改该配置后，需要重新加载插件。")
-      .addText((text) =>
-        text
-          .setPlaceholder("1000")
-          .setValue(this.plugin.settings.dailyTargetWordCount.toString())
-          .onChange(async (value) => {
-            try {
-              // 转换为整数
-              this.plugin.settings.dailyTargetWordCount = parseInt(value);
-            } catch (e) {
-              // 如果转换失败，则设置为默认值
-              console.error("设置每日目标，数据不为数字", e);
-            }
-            await this.plugin.saveSettings();
-          })
-      );
+    // new Setting(containerEl)
+    //   .setName("每日目标")
+    //   .setDesc("设置每日目标。修改该配置后，需要重新加载插件。")
+    //   .addText((text) =>
+    //     text
+    //       .setPlaceholder("1000")
+    //       .setValue(this.plugin.settings.dailyTargetWordCount.toString())
+    //       .onChange(async (value) => {
+    //         try {
+    //           // 转换为整数
+    //           this.plugin.settings.dailyTargetWordCount = parseInt(value);
+    //         } catch (e) {
+    //           // 如果转换失败，则设置为默认值
+    //           console.error("设置每日目标，数据不为数字", e);
+    //         }
+    //         await this.plugin.saveSettings();
+    //       })
+    //   );
   }
 }
 
